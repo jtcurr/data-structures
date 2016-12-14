6a49bea117069b54f7b26f8f277517e25e033ff0
jtcurr <johntcurrie@gmail.com>
2016-12-14 14:38:24 -0800

    stack shared behavior, functional inst. style, stack-specific behavior pass

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,22 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    var value = someInstance[size - 1];
+    delete someInstance[size - 1];
+    if (size > 0) { size--; }
+    return value;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
