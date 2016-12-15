696934190c85e85bc81f46dc4a734f00cae0f0ce
jtcurr <johntcurrie@gmail.com>
2016-12-14 16:07:49 -0800

    Prototypal Queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
+  var snacks = Object.create(queueMethods);
+  snacks.length = 0;
+  return snacks;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
+var queueMethods = {
+  'enqueue': function(value) {
+    this[this.length] = value;
+    this.length++;
+  },
+  'dequeue': function() {
+    var result = this[0];
+    for (var i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+    if (this.length > 0) {
+      this.length--;
+    }
+    return result;
+  },
+  'size': function () {
+    return this.length;
+  }
 
-var queueMethods = {};
+};
 
 
