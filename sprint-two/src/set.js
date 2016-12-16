62f7bc46feb14c2a099e0109efbe6d27805ab2d2
jtcurr <johntcurrie@gmail.com>
2016-12-16 10:29:36 -0800

    Finished Set Tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  var key = Object.keys(this._storage);
+  for (var i = 0; i < key.length; i++) {
+    if (key[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
