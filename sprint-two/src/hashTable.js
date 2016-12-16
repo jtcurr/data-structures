2bb9cc8981765b53f67bfae8be7cb58d9f366832
jtcurr <johntcurrie@gmail.com>
2016-12-16 14:18:51 -0800

    Better finished hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,47 +9,47 @@ HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var pair = [k, v];
   var found = false;
-  if (this._storage[index] === undefined) {
-    this._storage[index] = [];
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
   }
-
-  for (var x = 0; x < this._storage[index].length; x++) {
-    if (this._storage[index][x][0] === k) {
-      this._storage[index][x][1] = v;
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      this._storage.get(index)[i][1] = v;
       found = true;
     }
-  } 
-  if (!found) { 
-    this._storage[index].push(pair); 
   }
+  if (!found) {
+    this._storage.get(index).push(pair);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      return this._storage[index][i][1];
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      return this._storage.get(index)[i][1];
     }
   }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      this._storage[index].splice(i, 1);
+  for (var i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i][0] === k) {
+      this._storage.get(index).splice(i, 1);
     }
   }
 };
 
-var getIndexBelowMaxForKey = function (item, max) {
-  var result = 0;
-  for (var i = 0; i < item.length; i++) {
-    var char = item.charCodeAt(i);
-    result = (result ^ char) + result;
-  }
-  return result % max;
-};
+// var getIndexBelowMaxForKey = function (item, max) {
+//   var result = 0;
+//   for (var i = 0; i < item.length; i++) {
+//     var char = item.charCodeAt(i);
+//     result = (result ^ char) + result;
+//   }
+//   return result % max;
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
